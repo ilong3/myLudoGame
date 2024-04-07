17d84b044b7bddab12fc9a5fd040ba03c60ab918
unknown <mammanilong3@gmail.com>
2024-04-07 03:42:00 -0700

    first commit

---
+++ scope.js
@@ -0,0 +1,19 @@
+const name = "jonas";
+function first(){
+    const age = 30;
+     if(age >=30){
+        const decades = 3;
+        var millenial =true;
+     }
+
+
+
+     function second(){
+        const job ="Teacher";
+        console.log(`${name} is a ${age} old ${job}`)
+     }
+
+     second()
+
+}
+first();
